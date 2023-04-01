8e804c95dab874d264266fc0449705145c45e3a2
Mi-le-na <milena.035@outlook.com>
2023-04-01 19:48:56 -0300

    Queda, som e voltar para tela inicial.

--- jogo.js
+++ jogo.js
@@ -1,5 +1,9 @@
 console.log('[Mi-le-na] Flappy Bird');
 
+
+const som_hit = new Audio();
+som_hit.src = './efeitos/hit.wav';
+
 const sprites = new Image();
 sprites.src = './sprites.png';
 
@@ -17,7 +21,7 @@ const planoDeFundo = {
   y: canvas.height - 204,
   desenha() {
     contexto.fillStyle = '#70c5ce';
-    contexto.fillRect(0,0, canvas.width, canvas.height)
+    contexto.fillRect(0, 0, canvas.width, canvas.height)
 
     contexto.drawImage(
       sprites,
@@ -64,40 +68,64 @@ const chao = {
   },
 };
 
-const flappyBird = {
-  spriteX: 0,
-  spriteY: 0,
-  largura: 33,
-  altura: 24,
-  x: 10,
-  y: 50,
-  gravidade: 0.25,
-  velocidade: 0,
-  atualiza() {
-    flappyBird.velocidade = flappyBird.velocidade + flappyBird.gravidade;
-    flappyBird.y = flappyBird.y + flappyBird.velocidade;
-  },
-  desenha() {
-    contexto.drawImage(
-      sprites,
-      flappyBird.spriteX, flappyBird.spriteY, // Sprite X, Sprite Y
-      flappyBird.largura, flappyBird.altura, // Tamanho do recorte na sprite
-      flappyBird.x, flappyBird.y,
-      flappyBird.largura, flappyBird.altura,
-    );
+function fazcolisao(flappyBird, chao) {
+  const flappyBirdy = flappyBird.y + flappyBird.altura;
+  const chaoy = chao.y;
+
+  if (flappyBirdy >= chaoy) {
+    return true;
+
   }
+  return false;
 }
+function criaflappybird() {
+  const flappyBird = {
+    spriteX: 0,
+    spriteY: 0,
+    largura: 33,
+    altura: 24,
+    x: 10,
+    y: 50,
+    pulo: 4.6,
+    pula() {
+      console.log('devopular');
+      flappyBird.velocidade = -flappyBird.pulo;
+    },
+    gravidade: 0.25,
+    velocidade: 0,
 
+    atualiza() {
+      if (fazcolisao(flappyBird, chao)) {
+        console.log('fez colisao');
+        som_hit.play();
 
-
-
+        setTimeout(() => {
+        mudaparatela(telas.inicio);
+      }, 500);
+        return;
+      }
+      flappyBird.velocidade = flappyBird.velocidade + flappyBird.gravidade;
+      flappyBird.y = flappyBird.y + flappyBird.velocidade;
+    },
+    desenha() {
+      contexto.drawImage(
+        sprites,
+        flappyBird.spriteX, flappyBird.spriteY, // Sprite X, Sprite Y
+        flappyBird.largura, flappyBird.altura, // Tamanho do recorte na sprite
+        flappyBird.x, flappyBird.y,
+        flappyBird.largura, flappyBird.altura,
+      );
+    }
+  }
+  return flappyBird;
+}
 const mensagemGetReady = {
-  sx:134,
-  sy:0,
-  w:174,
-  h:152,
-  x: (canvas.width / 2) - 174/2,
-  y:50,
+  sx: 134,
+  sy: 0,
+  w: 174,
+  h: 152,
+  x: (canvas.width / 2) - 174 / 2,
+  y: 50,
   desenha() {
     contexto.drawImage(
       sprites,
@@ -109,18 +137,26 @@ const mensagemGetReady = {
   }
 }
 
-
 // telas
-let telaativa= {};
-function mudaparatela (novatela) {
-telaativa = novatela
+const globais = {};
+let telaativa = {};
+function mudaparatela(novatela) {
+  telaativa = novatela;
+  if (telaativa.inicializa) {
+    telaativa.inicializa();
+  }
 }
-const telas= {
+const telas = {
   inicio: {
+    
+    inicializa() {
+      globais.flappyBird = criaflappybird();
+       
+    },
     desenha() {
       planoDeFundo.desenha();
       chao.desenha();
-      flappyBird.desenha();
+      globais.flappyBird.desenha();
       mensagemGetReady.desenha();
     },
     click() {
@@ -132,29 +168,30 @@ const telas= {
   }
 };
 
-telas.jogo ={
+telas.jogo = {
   desenha() {
     planoDeFundo.desenha();
     chao.desenha();
-    flappyBird.desenha();
+    globais.flappyBird.desenha();
   },
- 
+  click() {
+    globais.flappyBird.pula();
+  },
   atualiza() {
-    flappyBird.atualiza();
-  } 
+    globais.flappyBird.atualiza();
+  }
 };
 
 function loop() {
- 
   telaativa.desenha();
   telaativa.atualiza();
 
   requestAnimationFrame(loop);
 }
 
-window.addEventListener('click', function() {
-  if(telaativa.click){
-  telaativa.click();
+window.addEventListener('click', function () {
+  if (telaativa.click) {
+    telaativa.click();
   }
 });
 mudaparatela(telas.inicio);
